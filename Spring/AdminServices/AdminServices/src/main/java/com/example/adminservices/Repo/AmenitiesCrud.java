7457719c5fe290745b49808bb1f2fb0ffac60043
Sweta Jaiswal <89539041+Jaiswal-Sweta@users.noreply.github.com>
2022-04-20 13:38:38 +0530

    Delete Spring/AdminServices/AdminServices directory

--- Spring/AdminServices/AdminServices/src/main/java/com/example/adminservices/Repo/AmenitiesCrud.java
+++
@@ -1,8 +0,0 @@
-package com.example.adminservices.Repo;
-
-import com.example.adminservices.Model.AmenitiesModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AmenitiesCrud extends CrudRepository<AmenitiesModel,Integer> {
-
-}
--- Spring/AdminServices/AdminServices/src/main/java/com/example/adminservices/Repo/EventsCrud.java
+++
@@ -1,8 +0,0 @@
-package com.example.adminservices.Repo;
-
-import com.example.adminservices.Model.EventsModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventsCrud extends CrudRepository<EventsModel,Integer> {
-
-}
