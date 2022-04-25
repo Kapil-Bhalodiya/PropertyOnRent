015ebc2fbe3d7285ebaa27bff7f56426519053cd
bhalk001 <kapilkumar.bhalodiya@vcontractor.co.za>
2022-04-25 12:45:49 +0530

    done

---
+++ Spring/Credential/src/main/java/com/example/credential/Model/ResponseData.java
@@ -0,0 +1,12 @@
+package com.example.credential.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseData {
+    private Boolean Successor;
+    private String Message;
+}
